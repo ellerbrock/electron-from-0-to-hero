95d15940ef74fdbd0ff714d3a93381da7e455fc5
Maik Ellerbrock <opensource@frapsoft.com>
2016-07-17 01:51:04 +0800

    add chapter 02 photon css

---
+++ 02-photon-css/src/index.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+require('electron-debug')()
+
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
+  mainWindow.loadURL(`file://${__dirname}/view/index.html`)
+  mainWindow.on('closed', function() {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function() {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
